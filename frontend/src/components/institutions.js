2d6a2a84d39d439f5328ac397eb9ce48af2bbd06
Vinícius Amorim <vamorim.dev@gmail.com>
2022-09-27 13:40:51 -0300

    table

--- frontend/src/components/institutions.js
+++ frontend/src/components/institutions.js
@@ -4,12 +4,27 @@ function Institutions(props) {
   return (
     <div>
       <h1>These Institutions are from the API</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Nome</th>
+            <th>CNPJ</th>
+            <th>Tipo</th>
+          </tr>
+        </thead>
+      </table>
       {props.institutions.map((institution) => {
         return (
           <div key={institution.name}>
-            <h2>Nome: {institution.name}</h2>
-            <p>CNPJ: {institution.cnpj}</p>
-            <p>Tipo: {institution.i_type}</p>
+            <table>
+              <tbody>
+                <tr>
+                  <td>{institution.name}</td>
+                  <td>{institution.cnpj}</td>
+                  <td>{institution.i_type}</td>
+                </tr>
+              </tbody>
+            </table>
           </div>
         );
       })}
--- frontend/src/components/students.js
+++ frontend/src/components/students.js
@@ -4,18 +4,7 @@ function Students(props) {
   return (
     <div>
       <h1>These students are from the API</h1>
-      {props.students.map((student) => {
-        return (
-          <div key={student.name}>
-            <h2>Nome: {student.name}</h2>
-            <p>Genero: {student.gender}</p>
-            <p>CPF: {student.cpf}</p>
-            <p>Nascimento: {student.birth}</p>
-            <p>Metodo de pagamento: {student.payment}</p>
-          </div>
-        );
-      })}
-      {/* <table>
+      <table>
         <thead>
           <tr>
             <th>Nome</th>
@@ -23,7 +12,6 @@ function Students(props) {
             <th>CPF</th>
             <th>Nascimento</th>
             <th>Metodo de Pagamento</th>
-            <th colspan="1"></th>
           </tr>
         </thead>
       </table>
@@ -40,7 +28,10 @@ function Students(props) {
                   <td>{student.payment}</td>
                 </tr>
               </tbody>
-            </table> */}
+            </table>
+          </div>
+        );
+      })}
     </div>
   );
 }
